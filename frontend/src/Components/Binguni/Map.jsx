fe3f45299c75aea2fb8ababc2b55e777f14bbc01
bingers00 <it21826122@my.sliit.lk>
2024-03-26 00:47:25 +0530

    installingdependencies

--- frontend/src/Components/Binguni/Map.jsx
+++ frontend/src/Components/Binguni/Map.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {LoadScript} from '@react-google-maps/api'
+import Map from './CustomeGoogleMap'
+const libraries=['places']
+const GoogleMap = () => {
 
-export default function Map() {
   return (
     <div id='map'>
-      
+      <LoadScript googleMapsApiKey='AIzaSyBZq5ejEeybo_1qhiWHIJlC66CZ3mrUrUI' libraries={libraries}>
+<Map/>
+</LoadScript>
       </div>
   )
+
 }
+
+export default Map;
+
+
+ 
+
