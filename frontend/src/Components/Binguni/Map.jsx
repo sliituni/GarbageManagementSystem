f52b228635f03933ba2a9f9db4eaba8a159415ab
bingers00 <it21826122@my.sliit.lk>
2024-03-26 01:01:51 +0530

    corrections

--- frontend/src/Components/Binguni/Map.jsx
+++ frontend/src/Components/Binguni/Map.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {LoadScript} from '@react-google-maps/api'
-import Map from './CustomeGoogleMap'
+import CustomGoogleMap from './CustomeGoogleMap'
 const libraries=['places']
 const GoogleMap = () => {
 
   return (
     <div id='map'>
       <LoadScript googleMapsApiKey='AIzaSyBZq5ejEeybo_1qhiWHIJlC66CZ3mrUrUI' libraries={libraries}>
-<Map/>
+<CustomGoogleMap/>
 </LoadScript>
       </div>
   )
