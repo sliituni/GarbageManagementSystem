2bdbb775f712198632b8f21219e5c2f1dc1d545d
bingers00 <it21826122@my.sliit.lk>
2024-05-04 11:54:49 +0530

    bcommit

--- frontend/src/Components/Binguni/AllGarbageRequests.jsx
+++ frontend/src/Components/Binguni/AllGarbageRequests.jsx
@@ -3,45 +3,41 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 export default function AllGarbageRequests() {
-    const[garbageRequests, setGarbageRequests] = useState([]);
+    const [garbageRequests, setGarbageRequests] = useState([]);
 
     useEffect(() => {
         function getGarbageRequests() {
             axios.get("http://localhost:4011/garbageRequest/GarbafeRequest")
-
             .then((res) => {
                 setGarbageRequests(res.data);
             })
             .catch((err) => {
                 alert(err.message);
-            })
+            });
         }
 
         getGarbageRequests();
-    });
+    }, []); // Empty dependency array to execute only once on component mount
 
     return (
-        <div>
-            <div className="container">
         <div className="container">
-          <div className="container"><br/><br/><br/><br/>
+            <br/><br/><br/><br/>
             <table className="table">
-                <thead>
-                    <tr className="table-success">
-                        <th scope="col">name</th>
-                        <th scope="col">companyName</th>
-                        <th scope="col">address</th>
-                        <th scope="col">contactNo</th>
-                        <th scope="col">nic</th>
-                        <th scope="col">garbageType</th>
-                        <th scope="col">reason</th>
-                        <th scope="col">quantity</th>
+                <thead className="table-success">
+                    <tr>
+                        <th>Name</th>
+                        <th>Company Name</th>
+                        <th>Address</th>
+                        <th>Contact No</th>
+                        <th>NIC</th>
+                        <th>Garbage Type</th>
+                        <th>Reason</th>
+                        <th>Quantity</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {garbageRequests.map ((garbageRequest,index) => (
-                        <tr key = {garbageRequest._id}>
-                            <th scope="row">{index + 1}</th>
+                    {garbageRequests.map((garbageRequest, index) => (
+                        <tr key={index}>
                             <td>{garbageRequest.name}</td>
                             <td>{garbageRequest.companyName}</td>
                             <td>{garbageRequest.address}</td>
@@ -50,23 +46,10 @@ export default function AllGarbageRequests() {
                             <td>{garbageRequest.garbageType}</td>
                             <td>{garbageRequest.reason}</td>
                             <td>{garbageRequest.quantity}</td>
-                            <td className="d-flex justify-contenet-between">
-                            </td>
                         </tr>
-                    ) )}
-                    
-
-
-
+                    ))}
                 </tbody>
             </table>
-            
-            
-            </div>  
-            </div> 
         </div>
-
-        </div>
-    )
-
+    );
 }
